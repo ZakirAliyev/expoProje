537310516f05a5092b8776e868988193f4d5060d
ZakirAliyev <aliyevzakir814@gmail.com>
2024-12-06 09:10:41 -0800

    EXPO

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,8 +3,20 @@ import './index.scss'
 function About() {
     return (
         <section id={"about"}>
-            <h2>About</h2>
-            <p>Home Page / About</p>
+            <div className="container">
+                <h2 className={"h2"}>About</h2>
+                <p className={"p"}>Home Page / About</p>
+                <div className={"row"}>
+                    <div className={"col-6"}>
+                        <h2>About us</h2>
+                        <p>Şirkətimiz, ofisinizin gündəlik təchizatı üçün zəruri olan dəftərxana ləvazimatlarının, su,
+                            çay, kofe kimi daimi istifadə edilən qida məhsullarının, gigiyena və təmizlik vasitələrinin,
+                            daşınan və daşınmaz hər növ inventarın (ofis mebel dəstləri, kompyuter və onun yan
+                            avadanlıqları) Sizə sürətli və maneəsiz çatdırılması xidmətini həyata keçirir.</p>
+                    </div>
+                    <div className={"col-6"}>Salam</div>
+                </div>
+            </div>
         </section>
     );
 }
--- src/pages/ContactUs/index.jsx
+++ src/pages/ContactUs/index.jsx
@@ -1,6 +1,20 @@
 import './index.scss'
+import {useFormik} from 'formik';
 
 function ContactUs() {
+
+    const formik = useFormik({
+        initialValues: {
+            fullName: '',
+            title: '',
+            email: '',
+            message: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
     return (
         <section id={"contactUs"}>
             <div className={"container"}>
@@ -16,19 +30,42 @@ function ContactUs() {
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d759.8292178521255!2d49.84850038794004!3d40.379669215083226!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40307d0a3ea7028d%3A0xac9d73dbd66392c8!2s28%20May!5e0!3m2!1str!2saz!4v1733343820476!5m2!1str!2saz"
                             style={{
-                            border: 0
-                        }} allowFullScreen="" loading="lazy"
+                                border: 0
+                            }} allowFullScreen="" loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                     <div className={"col-6"}>
-                        <form>
+                        <form onSubmit={formik.handleSubmit}>
                             <h2>Application Form</h2>
-                            <input placeholder={"Name, surname"}/>
-                            <input placeholder={"Title"}/>
-                            <input placeholder={"E-mail"}/>
+                            <input
+                                required
+                                placeholder={"Name, surname"}
+                                name={"fullName"}
+                                onChange={formik.handleChange}
+                                value={formik.values.firstName}
+                            />
+                            <input
+                                required
+                                placeholder={"Title"}
+                                name={"title"}
+                                onChange={formik.handleChange}
+                                value={formik.values.firstName}
+                            />
+                            <input
+                                required
+                                type="email"
+                                placeholder={"E-mail"}
+                                name={"email"}
+                                onChange={formik.handleChange}
+                                value={formik.values.firstName}
+                            />
                             <textarea
+                                required
                                 placeholder={"Message"}
+                                name={"message"}
                                 rows={5}
+                                onChange={formik.handleChange}
+                                value={formik.values.firstName}
                             />
                             <button type={"submit"}>Send</button>
                         </form>
