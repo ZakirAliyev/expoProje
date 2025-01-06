876913d9d054d890b911eedac85780081d9a662f
ZakirAliyev <aliyevzakir814@gmail.com>
2025-01-07 03:26:34 +0400

    EXPO

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,54 @@
-import './index.scss'
+import React, { useState, useEffect } from "react";
+import "./index.scss";
 import Banner from "../../components/Banner/index.jsx";
 import Sections from "../../components/Sections/index.jsx";
 import Brands from "../../components/Brands/index.jsx";
 import CategoryWrapper from "../../components/CategoryWrapper/index.jsx";
 import Sections1 from "../../components/Sections1/index.jsx";
-import {Helmet} from "react-helmet-async";
+import { Helmet } from "react-helmet-async";
+import logo from "/src/assets/logo.png";
+import AnyLoading from "../../components/AnyLoading/index.jsx";
 
 function Home() {
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        // LocalStorage kontrolü
+        const hasVisited = localStorage.getItem("hasVisited");
+        if (!hasVisited) {
+            setIsLoading(true);
+            localStorage.setItem("hasVisited", "true");
+        }
+
+        if (isLoading) {
+            const timer = setTimeout(() => {
+                setIsLoading(false);
+            }, 2000); // 2 saniye bekleme süresi
+            return () => clearTimeout(timer);
+        }
+    }, [isLoading]);
+
     return (
-        <section id={"home"}>
-            <Helmet>
-                <title>ExpoHome - Ana Səhifə</title>
-            </Helmet>
-            <Banner/>
-            <Brands/>
-            <Sections/>
-            <CategoryWrapper/>
-            <Sections1/>
-        </section>
+        <div className={isLoading ? "home-container loading" : "home-container"}>
+            {isLoading && (
+                <div className="loading-screen">
+                    <div className="loading-content">
+                        <img src={logo} alt={"Logo"} />
+                        <AnyLoading />
+                    </div>
+                </div>
+            )}
+            <section id="home">
+                <Helmet>
+                    <title>ExpoHome - Ana Səhifə</title>
+                </Helmet>
+                <Banner />
+                <Brands />
+                <Sections />
+                <CategoryWrapper />
+                <Sections1 />
+            </section>
+        </div>
     );
 }
 
